e4c8e83ecf96967fc48e5a27e7f9c6216dd38c4b
Ricardo Barreira <Rbbarreira@hotmail.com>
2024-05-04 16:59:56 +0100

    Create Domain Models

---
+++ Online Recipes-Domain/Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Online_Recipes_Domain.Models
+{
+    public abstract class Entity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
---
+++ Online Recipes-Domain/Models/Rating.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Online_Recipes_Domain.Models
+{
+    public class Rating : Entity
+    {
+        [RegularExpression("[0-5]")]
+        public int Star { get; set; } = 0;
+
+        [JsonIgnore]
+        public ICollection<Recipe> Recipes { get; set; }
+    }
+}
