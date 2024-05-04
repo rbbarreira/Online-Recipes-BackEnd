fc19e553db5c9d485f39bc22e6576606a6e8d2ed
Ricardo Barreira <Rbbarreira@hotmail.com>
2024-05-04 17:06:38 +0100

    Create Domain Dtos

---
+++ Online Recipes-Domain/DTOs/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Online_Recipes_Domain.Models;
+
+namespace Online_Recipes_Domain.DTOs
+{
+    public class UserDto : Entity
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string? UserName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Email is required")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [NotMapped]
+        public string Password { get; set; } = string.Empty;
+    }
+}
