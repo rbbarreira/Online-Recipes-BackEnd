690ba3e0d55def2896e57ad74f00da0439616cdd
Ricardo Barreira <Rbbarreira@hotmail.com>
2024-05-04 17:03:36 +0100

    Create Domain Account

---
+++ Online Recipes-Domain/Account/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace Online_Recipes_Domain.Account
+{
+    public static class Role
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public const string Guest = "Guest";
+    }
+}
---
+++ Online Recipes-Domain/Account/UserToken.cs
@@ -0,0 +1,7 @@
+﻿namespace Online_Recipes_Domain.Account
+{
+    public class UserToken
+    {
+        public string Token { get; set; }
+    }
+}
