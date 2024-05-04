d8e1f41a5882684dc56129c9751f96244f53d6f8
Ricardo Barreira <Rbbarreira@hotmail.com>
2024-05-04 17:55:55 +0100

    Implementando as Interfaces e os metodos para gerir a API

---
+++ Online-Recipes-Repository/Repo-Implementations/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Online_Recipes_Data.Context;
+using Online_Recipes_Domain.Models;
+using Online_Recipes_Repository.Repo_Interfaces;
+
+namespace Online_Recipes_Repository.Repo_Implementations
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ApplicationContext applicationContext) : base(applicationContext)
+        {
+
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoryByName(string name)
+        {
+            return await _dbSet.Where(x => x.Name.ToLower().Contains(name.ToLower())).ToListAsync();
+        }
+    }
+}
---
+++ Online-Recipes-Repository/Repo-Implementations/IngredientRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Online_Recipes_Data.Context;
+using Online_Recipes_Domain.Models;
+using Online_Recipes_Repository.Repo_Interfaces;
+
+namespace Online_Recipes_Repository.Repo_Implementations
+{
+    public class IngredientRepository : Repository<Ingredient>, IIngredientRepository
+    {
+        public IngredientRepository(ApplicationContext applicationContext) : base(applicationContext)
+        {
+
+        }
+
+        public async Task<IEnumerable<Ingredient>> GetIngredientByName(string product)
+        {
+            return await _dbSet.Where(x => x.Product.ToLower().Contains(product.ToLower())).ToListAsync();
+        }
+
+        public async Task<List<Ingredient>> GetAll()
+        {
+            return await _dbSet.Include(a => a.Ingredient_Quantities).ToListAsync();
+        }
+    }
+}
