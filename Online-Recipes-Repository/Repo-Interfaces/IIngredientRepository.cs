f39b384bc9beffc5a511e046bc2aacbff4eaab8b
Ricardo Barreira <Rbbarreira@hotmail.com>
2024-05-04 17:43:53 +0100

    Criando os Interfaces dos Repositorios

---
+++ Online-Recipes-Repository/Repo-Interfaces/IIngredientRepository.cs
@@ -0,0 +1,10 @@
+﻿using Online_Recipes_Domain.Models;
+
+namespace Online_Recipes_Repository.Repo_Interfaces
+{
+    public interface IIngredientRepository : IRepository<Ingredient>
+    {
+        //Procura um Ingrediente pelo Produto(nome)
+        Task<IEnumerable<Ingredient>> GetIngredientByName(string product);
+    }
+}
---
+++ Online-Recipes-Repository/Repo-Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Online_Recipes_Domain.Account;
+
+namespace Online_Recipes_Repository.Repo_Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> Add(User user);
+
+        Task<bool> EmailExist(string? email);
+
+        Task<bool> UsernameExist(string? username);
+
+        Task<bool> Authenticate(string email, string password);
+
+        public Task<User> GetUserByEmail(string email);
+
+        public string GenerateToken(User user);
+    }
+}
